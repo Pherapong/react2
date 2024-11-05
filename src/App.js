f15801e258d7b3358ba523eb12b1b9f5e3acc5d8
pherapong <pherapongnsk@gmaile.com>
2024-11-05 22:57:57 +0700

    4

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import Transaction from "./components/Transaction";
 import FormConponent from "./components/FormConponent";
 import './App.css'
-import { useState,useEffect,useReducer} from "react";
+import { useState,useEffect} from "react";
 import DataContext from "./data/DataContext";
-import ReportComponent from "./components/ReportComponent";
+import ReportComponent from "./components/ReportComponent"
+import {BrowserRouter as Router,Routes,Switch,Route,Link} from "react-router-dom";
+
 function App() {
   const desight = {color:"red",textAlign:"center",fontSize:'1.5rem'}
-
-  // const initState = [
-  //       {id:1,title:"รายได้",amount:150000},
-  //       {id:2,title:"ค่าเช่า",amount:-5000},
-  // ]
   
-  const [items,setItems] = useState([])
-
-const [reportIncome,setReportIncome] = useState(0)
-const [reportExpense,setReportExpense] = useState(0)
-const onAddnewItem = (newItem) => {
+  const initData = [
+    {id:1,title:"home",amount:-2000},
+    {id:1,title:"income",amount:50000},
+  ]
+  const [items,setItems] = useState(initData)
+  const [reportIncome,setReportIncome] = useState(0)
+  const [reportExpense,setReportExpense] = useState(0)
+  const onAddnewItem = (newItem) => {
       setItems((prevItem) =>{
         return [newItem,...prevItem]
       })
@@ -25,36 +25,37 @@ useEffect(()=>{
     const amounts = items.map(items=>items.amount)
     const income = amounts.filter(element=>element>0).reduce((total,element)=>total+=element,0)
     const expense = (amounts.filter(element=>element<0).reduce((total,element)=>total+=element,0))*-1
-   setReportIncome(income)
-   setReportExpense(expense)
+   setReportIncome(income.toFixed(2))
+   setReportExpense(expense.toFixed(2))
 },[items,reportIncome,reportExpense])
-
-    //reducer state
-    const [showReport,setShowReport] = useState(false)
-    const reducer = (state,action)=>{
-        switch(action.type){
-          case "SHOW" :
-            return setShowReport(true)
-          case "HIDE" :
-            return setShowReport(false)
-        }
-    }
-  const [result,dispatch] = useReducer(reducer,showReport)
   return (
-   <DataContext.Provider value={
-    {
-      income : reportIncome,
-      expense : reportExpense
-    }
-   }>
+   <DataContext.Provider value={{income : reportIncome,expense : reportExpense}}>
      <div className="container"> 
         <h1 style={desight}>แอพบัญชีรายรับ - รายจ่าย</h1>
-        {showReport && <ReportComponent/>}
+        <Router>
+        <div>
+          <ul className ="horizontal-menu">
+            <li>
+              <Link to="/">ข้อมูลบัญชี</Link>
+            </li>
+            <li>
+              <Link to="/insert">บันทึกข้อมูล</Link>
+            </li>
+          </ul> 
+         {/* <Routes>
+            <Route path="/" exact>
+               <ReportComponent/>
+            </Route>
+            <Route path="/insert">
+               <FormConponent onAddItem = {onAddnewItem}/>
+               <Transaction items = {items}/>
+           </Route>
+          </Routes> */}
+        </div>
+        </Router>
+        <ReportComponent/>
         <FormConponent onAddItem = {onAddnewItem}/>
         <Transaction items = {items}/>
-      <h1>{result}</h1>
-      <button onClick={()=>dispatch({type:"SHOW"})}>แสดง</button>
-      <button onClick={()=>dispatch({type:"HIDE"})}>ซ่อน</button>
     </div> 
    </DataContext.Provider>  
     ); 
