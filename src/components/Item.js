30a261ee4753f9f89634ea389fd0b9d098d30a45
pherapong <pherapongnsk@gmaile.com>
2024-11-05 22:58:01 +0700

    5

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,12 +1,14 @@
 import PropTypes from 'prop-types';
-import { useContext } from 'react';
 import './Item.css'
 const Item = (props) => {
     const{title,amount}=props
     const status = amount < 0? "expense":"income"
     const symbol = amount < 0? "-":"+"
+    const formatNumber=(num)=> {
+        return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+    }
     return (
-        <li className = {status}> {title} <span> {symbol}{Math.abs(amount)} </span> </li>
+        <li className = {status}> {title} <span> {symbol}{formatNumber(Math.abs(amount))} </span> </li>
     );
 }
 
